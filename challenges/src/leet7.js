befa3bec41ea390f7c8a66ec9679d21508f0c52c
Curtis Mumby <curtis.mumby@gmail.com>
2024-04-04 22:46:59 -0400

    feat: completed challenge "Reverse Integer"

---
+++ challenges/src/leet7.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-param-reassign */
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+function reverse(x) {
+  const IS_NEGATIVE = x < 0;
+  if (IS_NEGATIVE) {
+    x = Math.abs(x);
+  }
+  let numberArray = x.toString().split('');
+  console.log({ numberArray });
+  let numberLength = x.toString().length;
+
+  numberArray = numberArray.map(digit => {
+    digit = numberArray[numberLength - 1];
+    numberLength -= 1;
+    return digit;
+  });
+
+  let reversedNumber = parseInt(numberArray.join(''), 10);
+
+  reversedNumber = IS_NEGATIVE ? reversedNumber - reversedNumber - reversedNumber : reversedNumber;
+
+  return reversedNumber;
+}
+
+console.log(reverse(-123));
