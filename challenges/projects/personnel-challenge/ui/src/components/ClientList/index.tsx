344ea65a5dd8ce76ebc37266c97a9541af88282a
Curtis Mumby <curtismumby@Curtiss-Mac-mini.local>
2025-06-07 01:41:07 -0400

    feat: integrarte client api

--- challenges/projects/personnel-challenge/ui/src/components/ClientList/index.tsx
+++ challenges/projects/personnel-challenge/ui/src/components/ClientList/index.tsx
@@ -1,13 +1,45 @@
-export default function ClienList({ clients }) {
+type Client = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  birthDate: string;
+  policyNumber: string;
+  perimum: number;
+  address: string;
+}
+
+type ClienListProps = {
+  clients: Client[];
+}
+
+export default function ClienList({ clients }: ClienListProps) {
+//  console.log('ClienList: Rendering client list', clients);
   return (
-    <section className="flex w-full h-full">
-      <ul className="text-black">
-        <li>Homer Simpson</li>
-        <li>Marge Simpson</li>
-        <li>Bart Simpson</li>
-        <li>Lisa Simpson</li>
-        <li>Maggie Simpson</li>
+    <section className=" w-full h-full ">
+      <ul className="text-black divide-y divide-gray-300">
+        {
+          clients.map((client) => (
+            <li key={client.id} className="hover:bg-blue-100 py-2">
+              {client.firstName} {client.lastName}
+            </li>
+          ))
+        }
+
       </ul>
+      <div className="flex py-4 bg-gray-100 gap-2">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+          Previous
+        </button>
+        <button className="bg-green-500 text-white px-4 py-2 rounded">
+          Next
+        </button>
+      </div>
+      <div className="text-center text-gray-500 text-sm">
+        Page 1 of 10<br />
+        Showing 1-10 of 100
+      </div>
     </section>
   );
 }
--- challenges/projects/personnel-challenge/ui/src/components/ClientPanel/index.tsx
+++ challenges/projects/personnel-challenge/ui/src/components/ClientPanel/index.tsx
@@ -1,12 +1,75 @@
 import ClienList from '@/components/ClientList';
-export default function ClientPanel() {
+
+
+export default async function ClientPanel() {
+  const res = await fetch('http://localhost:3001/clients?page=1');
+  if (!res.ok) { return <div>Error loading clients</div>; }
+  const json = await res.json();
+  const data = json.data;
+  const featuredPerson = data[1]
+
   return (
     <section className="flex w-full h-full">
       <div className="w-1/4 bg-gray-100 p-4">
-        <ClienList />
+        <ClienList clients={data} />
       </div>
-      <div className="w-3/4 bg-white p-4">
-        <h3 className="text-black font-bold">Hello Alice!</h3>
+      <div className="w-3/4 text-center bg-white p-4">
+        <h2 className="text-black font-bold">Policy Details: {featuredPerson.firstName} {featuredPerson.lastName}</h2>
+        <div className="odd:bg-white even:bg-gray-100">
+          <dl className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 p-4">
+            <dt className="font-medium text-gray-600">First</dt>
+            <dd className="text-gray-900">{featuredPerson.firstName}</dd>
+          </dl>
+        </div>
+        <div className="odd:bg-white even:bg-gray-100">
+          <dl className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 p-4">
+            <dt className="font-medium text-gray-600">Last</dt>
+            <dd className="text-gray-900">{featuredPerson.lastName}</dd>
+          </dl>
+        </div>
+        <div className="odd:bg-white even:bg-gray-100">
+          <dl className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 p-4">
+            <dt className="font-medium text-gray-600">Email</dt>
+            <dd className="text-gray-900"><a href="mailto:bob.robertson@example.com">{featuredPerson.email}</a></dd>
+          </dl>
+        </div>
+        <div className="odd:bg-white even:bg-gray-100">
+          <dl className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 p-4">
+            <dt className="font-medium text-gray-600">Phone</dt>
+            <dd className="text-gray-900"><a href="mailto:bob.robertson@example.com">{featuredPerson.phone}</a></dd>
+          </dl>
+        </div>
+        <div className="odd:bg-white even:bg-gray-100">
+          <dl className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 p-4">
+            <dt className="font-medium text-gray-600">Birthdate</dt>
+            <dd className="text-gray-900"><a href="mailto:bob.robertson@example.com">{featuredPerson.birthDate}</a></dd>
+          </dl>
+        </div>
+        <div className="odd:bg-white even:bg-gray-100">
+          <dl className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 p-4">
+            <dt className="font-medium text-gray-600">Policy Number</dt>
+            <dd className="text-gray-900"><a href="mailto:bob.robertson@example.com">{featuredPerson.policyNumber}</a></dd>
+          </dl>
+        </div>
+        <div className="odd:bg-white even:bg-gray-100">
+          <dl className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 p-4">
+            <dt className="font-medium text-gray-600">Coverage Amount</dt>
+            <dd className="text-gray-900"><a href="mailto:bob.robertson@example.com">{featuredPerson.coverageAmount}</a></dd>
+          </dl>
+        </div>
+        <div className="odd:bg-white even:bg-gray-100">
+          <dl className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 p-4">
+            <dt className="font-medium text-gray-600">Permium</dt>
+            <dd className="text-gray-900"><a href="mailto:bob.robertson@example.com">{featuredPerson.premium}</a></dd>
+          </dl>
+        </div>
+        <div className="odd:bg-white even:bg-gray-100">
+          <dl className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 p-4">
+            <dt className="font-medium text-gray-600">Address</dt>
+            <dd className="text-gray-900"><a href="mailto:bob.robertson@example.com">{featuredPerson.address}</a></dd>
+          </dl>
+        </div>
+
       </div>
     </section>
   );
